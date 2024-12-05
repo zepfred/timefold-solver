e89540d3a117de8a62f386743693d6d73bc2c29f
Fred <zepfred@gmail.com>
2024-12-04 21:47:37 -0300

    chore: minor improvements

--- core/src/main/java/ai/timefold/solver/core/impl/localsearch/decider/acceptor/lateacceptance/DiversifiedLateAcceptanceAcceptor.java
+++ core/src/main/java/ai/timefold/solver/core/impl/localsearch/decider/acceptor/lateacceptance/DiversifiedLateAcceptanceAcceptor.java
@@ -1,6 +1,7 @@
 package ai.timefold.solver.core.impl.localsearch.decider.acceptor.lateacceptance;
 
 import java.time.Clock;
+import java.util.Arrays;
 
 import ai.timefold.solver.core.api.score.Score;
 import ai.timefold.solver.core.impl.localsearch.scope.LocalSearchMoveScope;
@@ -15,10 +16,14 @@ public class DiversifiedLateAcceptanceAcceptor<Solution_> extends LateAcceptance
     // Number of occurrences of lateWorse in the late elements
     protected int lateWorseOccurrences = -1;
     // Geometric restart
+    private static final int RESTART_LIST_SIZE = 50;
     private static final double GEOMETRIC_FACTOR = 1.3;
     private static final double SCALING_FACTOR = 1.0;
+    private Score<?> initialScore;
+    private boolean resized;
     private Score<?> bestRestartScore;
     private boolean restartTriggered;
+    private boolean restarted;
     private double geometricGrowFactor;
     private long nextRestart;
     private long lastImprovementMillis;
@@ -32,10 +37,12 @@ public class DiversifiedLateAcceptanceAcceptor<Solution_> extends LateAcceptance
         super.phaseStarted(phaseScope);
         lateWorseOccurrences = lateAcceptanceSize;
         lateWorse = phaseScope.getBestScore();
+        initialScore = lateWorse;
         bestRestartScore = phaseScope.getBestScore();
         geometricGrowFactor = 1;
         nextRestart = 1_000;
         restartTriggered = false;
+        restarted = false;
         lastImprovementMillis = clock.millis();
     }
 
@@ -46,12 +53,29 @@ public class DiversifiedLateAcceptanceAcceptor<Solution_> extends LateAcceptance
                 lastImprovementMillis = clock.millis();
                 this.bestRestartScore = moveScope.getScore();
                 this.restartTriggered = false;
-                logger.debug("DLAS restarted with {} - {}", moveScope.getScore(), moveScope.getMove());
+                this.restarted = true;
+                logger.info("DLAS restarted with {} - {}", moveScope.getScore(), moveScope.getMove());
+                if (!resized) {
+                    var newLateAcceptanceSize = RESTART_LIST_SIZE;
+                    var newPreviousScores = new Score[newLateAcceptanceSize];
+                    var pos = 0;
+                    for (var i = 0; i < newLateAcceptanceSize; i++) {
+                        newPreviousScores[i] = previousScores[pos];
+                        pos = (pos + 1) % lateAcceptanceSize;
+                    }
+                    logger.info("DLAS list resized from {} to {}", lateAcceptanceSize, newLateAcceptanceSize);
+                    this.lateWorse = initialScore;
+                    Arrays.fill(previousScores, lateWorse);
+                    this.lateWorseOccurrences = lateAcceptanceSize;
+                    this.lateAcceptanceSize = newLateAcceptanceSize;
+                    this.previousScores = newPreviousScores;
+                    this.resized = true;
+                }
             }
             return accept;
         }
         if (lastImprovementMillis > 0 && clock.millis() - lastImprovementMillis >= nextRestart) {
-            logger.debug("Restarting DLAS with geometric factor {}", geometricGrowFactor);
+            logger.info("Restarting DLAS with geometric factor {}", geometricGrowFactor);
             nextRestart = (long) Math.ceil(SCALING_FACTOR * geometricGrowFactor * 1_000);
             geometricGrowFactor = Math.ceil(geometricGrowFactor * GEOMETRIC_FACTOR);
             lastImprovementMillis = clock.millis();
@@ -59,6 +83,21 @@ public class DiversifiedLateAcceptanceAcceptor<Solution_> extends LateAcceptance
             return acceptRestartMove(moveScope);
         }
         restartTriggered = false;
+        return false;
+    }
+
+    private boolean applyRestart2(LocalSearchMoveScope<Solution_> moveScope) {
+        if (lastImprovementMillis > 0 && clock.millis() - lastImprovementMillis >= nextRestart
+                && moveScope.getScore().isFeasible()) {
+            logger.info("Restarting DLAS with geometric factor {}", geometricGrowFactor);
+            nextRestart = (long) Math.ceil(SCALING_FACTOR * geometricGrowFactor * 1_000);
+            geometricGrowFactor = Math.ceil(geometricGrowFactor * GEOMETRIC_FACTOR);
+            lastImprovementMillis = clock.millis();
+            this.lateWorse = moveScope.getStepScope().getPhaseScope().getBestScore();
+            Arrays.fill(previousScores, lateWorse);
+            this.lateWorseOccurrences = lateAcceptanceSize;
+            return true;
+        }
         return false;
     }
 
@@ -70,7 +109,8 @@ public class DiversifiedLateAcceptanceAcceptor<Solution_> extends LateAcceptance
     }
 
     private boolean acceptRestartMove(LocalSearchMoveScope<Solution_> moveScope) {
-        return compare(moveScope.getScore(), bestRestartScore) != 0 && moveScope.getScore().isFeasible();
+        return moveScope.getScore().isFeasible();
+        //        return compare(moveScope.getScore(), bestRestartScore) < 0 && moveScope.getScore().isFeasible();
     }
 
     @Override
@@ -82,8 +122,9 @@ public class DiversifiedLateAcceptanceAcceptor<Solution_> extends LateAcceptance
         var current = moveScope.getStepScope().getPhaseScope().getLastCompletedStepScope().getScore();
         var previous = current;
         var accept = compare(moveScore, current) == 0 || compare(moveScore, lateWorse) > 0;
-        updateRestartTime(moveScope);
-        if (accept || applyRestart(moveScope)) {
+        //        updateRestartTime(moveScope);
+        //        if (accept || applyRestart2(moveScope)) {
+        if (accept) {
             current = moveScore;
         }
         // Improves the diversification to allow the next iterations to find a better solution
@@ -95,6 +136,9 @@ public class DiversifiedLateAcceptanceAcceptor<Solution_> extends LateAcceptance
             updateLateScore(current);
         }
         lateScoreIndex = (lateScoreIndex + 1) % lateAcceptanceSize;
+        //        if (accept && compare(moveScore, moveScope.getStepScope().getPhaseScope().getBestScore()) > 0) {
+        //            logger.info("New best solution {}", moveScore);
+        //        }
         return accept;
     }
 
