188813eff909ae8f8349215718a68ae4f4cda79e
fred <zepfred@gmail.com>
2026-02-12 14:00:22 -0300

    ci: teste

--- core/src/main/java/ai/timefold/solver/core/api/domain/autodiscover/AutoDiscoverMemberType.java
+++ core/src/main/java/ai/timefold/solver/core/api/domain/autodiscover/AutoDiscoverMemberType.java
@@ -11,7 +11,7 @@ import ai.timefold.solver.core.api.domain.solution.ProblemFactProperty;
 /**
  * Determines if and how to automatically presume {@link ConstraintWeightOverrides},
  * {@link ProblemFactCollectionProperty}, {@link ProblemFactProperty}, {@link PlanningEntityCollectionProperty},
- * {@link PlanningEntityProperty} and {@link PlanningScore} annotations on {@link PlanningSolution} members
+ * {@link PlanningEntityProperty} and {@link PlanningScore} annotations on {@link PlanningSolution} membersasdf
  * based on the member type.xxx
  */
 public enum AutoDiscoverMemberType {
